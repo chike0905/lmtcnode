99ed27858f7c41ccec2996cf4f5945041fc7beb1
chike <chike@sfc.wide.ad.jp>
2017-07-19 14:23:42 +0900

    show live table from posted data from server

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { live: [
+      {"date":"2017/8/15(Thu.)",
+       "title":"Nobody Dies A Virgin,InFix,ANTIKNOCK pre.【Exist Episodes】",
+       "venue":"Shinjuku ANTIKNOCK",
+       "bands":"Layout my Torturechamber / Nobody Dies A Virgin / InFix / MASRAO / GIVEN BY THE FLAMES / おしゃめがね / EJECTS",
+       "open":"17:30",
+       "start":"18:00",
+       "adv":"2000yen",
+       "door":"2500yen"
+      },
+      {"date":"2017/8/15(Thu.)",
+       "title":"Nobody Dies A Virgin,InFix,ANTIKNOCK pre.【Exist Episodes】",
+       "venue":"Shinjuku ANTIKNOCK",
+       "bands":"Layout my Torturechamber / Nobody Dies A Virgin / InFix / MASRAO / GIVEN BY THE FLAMES / おしゃめがね / EJECTS",
+       "open":"17:30",
+       "start":"18:00",
+       "adv":"2000yen",
+       "door":"2500yen"
+      }
+      ]
+  });
 });
 
 module.exports = router;
